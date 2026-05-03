1b5ca9a40d8729083eae876aedb2a9b47c83efa7
Vitalii Zelinski <vetal.zelinski@gmail.com>
2026-05-03 11:55:45 +0300

    [ADD] new field and function

--- vz_color_tags_attribute/models/product_template_attribute_value.py
+++ vz_color_tags_attribute/models/product_template_attribute_value.py
@@ -6,3 +6,7 @@ class ProductTemplateAttributeValue(models.Model):
 
     color_hex = fields.Char(related="product_attribute_value_id.color_hex", string="Color Hex")
     tst_field = fields.Char('Test')
+    new_field = fields.Char('Test')
+
+    def function(self):
+        return 'Hello'
